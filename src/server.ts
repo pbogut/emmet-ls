7b80b782c6e8a22e59580d6952712af7788b72a0
Pawel Bogut <pbogut@pbogut.me>
2021-11-05 21:39:19 +0100

    Add settings for css / html filetypes
    Allow to select which snippet should be provided for given file type.

--- src/server.ts
+++ src/server.ts
@@ -136,21 +136,28 @@ connection.onInitialized(() => {
   }
 });
 
-// The example settings
-interface ExampleSettings {
-  maxNumberOfProblems: number;
+interface Settings {
+  html_filetypes: string[],
+  css_filetypes: string[],
 }
 
 // The global settings, used when the `workspace/configuration` request is not supported by the client.
 // Please note that this is not the case when using this server with the client provided in this example
 // but could happen with other clients.
-const defaultSettings: ExampleSettings = { maxNumberOfProblems: 1000 };
-let globalSettings: ExampleSettings = defaultSettings;
+const defaultSettings: Settings = {
+    html_filetypes: ['html'],
+    css_filetypes: ['css'],
+};
+let globalSettings: Settings = defaultSettings;
 
 // Cache the settings of all open documents
-let documentSettings: Map<string, Thenable<ExampleSettings>> = new Map();
+let documentSettings: Map<string, Thenable<Settings>> = new Map();
 
-function getDocumentSettings(resource: string): Thenable<ExampleSettings> {
+connection.onDidChangeConfiguration((change) => {
+      globalSettings = <Settings>change.settings;
+});
+
+function getDocumentSettings(resource: string): Thenable<Settings> {
   if (!hasConfigurationCapability) {
     return Promise.resolve(globalSettings);
   }
@@ -165,7 +172,84 @@ function getDocumentSettings(resource: string): Thenable<ExampleSettings> {
   return result;
 }
 
-documents.onDidClose((e) => {
+function getCssSnippet(abbreviation: string) {
+  const cssConfig = resolveConfig({
+    type: "stylesheet",
+    options: {
+      "output.field": (index : number, placeholder : string) =>
+        `\$\{${index}${placeholder ? ":" + placeholder : ""}\}`,
+    },
+  });
+  const markup = parseStylesheet(abbreviation, cssConfig);
+  return stringifyStylesheet(markup, cssConfig);
+}
+
+function getHtmlSnippet(abbreviation: string) {
+  const htmlconfig = resolveConfig({
+    options: {
+      "output.field": (index : number, placeholder : string) =>
+      `\$\{${index}${placeholder ? ":" + placeholder : ""}\}`,
+    },
+  });
+  const markup = parseMarkup(abbreviation, htmlconfig);
+  return stringifyMarkup(markup, htmlconfig);
+}
+
+function buildCompletionItem(abbreviation: string, textResult: string, range: any) {
+  return {
+    insertTextFormat: InsertTextFormat.Snippet,
+    label: abbreviation,
+    detail: abbreviation,
+    documentation: textResult,
+    textEdit: {
+      range,
+      newText: textResult,
+      // newText: textResult.replace(/\$\{\d*\}/g,''),
+    },
+    kind: CompletionItemKind.Snippet,
+    data: {
+      range,
+      textResult,
+    },
+  }
+}
+
+function buildRange(linenr: number, left: number, right: number) {
+  return {
+    start: {
+      line: linenr,
+      character: left,
+    },
+    end: {
+      line: linenr,
+      character: right,
+    },
+  };
+}
+
+function getCompeletionItem(linenr: number, line: string, char: number, opts? : any ) {
+  opts = typeof(opts) !== 'undefined' ? opts : {}
+
+  try {
+    let extractPosition = extract(line, char, opts);
+    if (extractPosition?.abbreviation != undefined) {
+      let left = extractPosition.start;
+      let right = extractPosition.start;
+      let abbreviation = extractPosition.abbreviation;
+
+      let textResult = opts.type == 'stylesheet'
+                     ? getCssSnippet(abbreviation)
+                     : getHtmlSnippet(abbreviation);
+
+      let range = buildRange(linenr, left, right);
+      return buildCompletionItem(abbreviation, textResult, range);
+    }
+  } catch (error) {
+    connection.console.log(`ERR: ${error}`);
+  }
+}
+
+documents.onDidClose((e : any) => {
   documentSettings.delete(e.document.uri);
 });
 
@@ -179,68 +263,22 @@ connection.onCompletion(
       let linenr = _textDocumentPosition.position.line;
       let line = String(content.split(/\r?\n/g)[linenr]);
       let character = _textDocumentPosition.position.character;
-      let extractPosition =
-        languageId != "css"
-          ? extract(line, character)
-          : extract(line, character, { type: "stylesheet" });
+      let result = [];
 
-      if (extractPosition?.abbreviation == undefined) {
-        throw "failed to parse line";
+      if (globalSettings.html_filetypes.indexOf(languageId) != -1) {
+        let completionItem = getCompeletionItem(linenr, line, character)
+        if (completionItem) {
+          result.push(completionItem);
+        }
+      }
+      if (globalSettings.css_filetypes.indexOf(languageId) != -1) {
+        let completionItem = getCompeletionItem(linenr, line, character, { type: "stylesheet" })
+        if (completionItem) {
+          result.push(completionItem);
+        }
       }
 
-      let left = extractPosition.start;
-      let right = extractPosition.start;
-      let abbreviation = extractPosition.abbreviation;
-      let textResult = "";
-      if (languageId === "html") {
-        const htmlconfig = resolveConfig({
-          options: {
-            "output.field": (index, placeholder) =>
-              `\$\{${index}${placeholder ? ":" + placeholder : ""}\}`,
-          },
-        });
-        const markup = parseMarkup(abbreviation, htmlconfig);
-        textResult = stringifyMarkup(markup, htmlconfig);
-      } else {
-        const cssConfig = resolveConfig({
-          type: "stylesheet",
-          options: {
-            "output.field": (index, placeholder) =>
-              `\$\{${index}${placeholder ? ":" + placeholder : ""}\}`,
-          },
-        });
-        const markup = parseStylesheet(abbreviation, cssConfig);
-        textResult = stringifyStylesheet(markup, cssConfig);
-      }
-      const range = {
-        start: {
-          line: linenr,
-          character: left,
-        },
-        end: {
-          line: linenr,
-          character: right,
-        },
-      };
-
-      return [
-        {
-          insertTextFormat: InsertTextFormat.Snippet,
-          label: abbreviation,
-          detail: abbreviation,
-          documentation: textResult,
-          textEdit: {
-            range,
-            newText: textResult,
-            // newText: textResult.replace(/\$\{\d*\}/g,''),
-          },
-          kind: CompletionItemKind.Snippet,
-          data: {
-            range,
-            textResult,
-          },
-        },
-      ];
+      return result;
     } catch (error) {
       connection.console.log(`ERR: ${error}`);
     }
